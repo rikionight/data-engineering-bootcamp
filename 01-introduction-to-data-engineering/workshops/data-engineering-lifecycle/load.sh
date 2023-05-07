9479cf88bdb41328321b706c35efc03ebd301ccd
Rikionight <35134541+rikionight@users.noreply.github.com>
2023-05-07 02:55:57 +0000

    Add connection

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/85d25a66-8fb7-4c23-a0d4-0aa1fea9d302/basket/randomDogs"
